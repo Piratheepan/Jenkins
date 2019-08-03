3069cf34d82b3443e7943ee7b8b8190f24488f01
Piratheepan <nkpratheepan@gmail.com>
2019-08-03 11:59:25 +0530

    First Commit

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+  public static void main(String[] args) {
+    int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+	System.out.println(myNumbers.length);
+    for (int i = 0; i < myNumbers.length; ++i) {
+		
+	System.out.println(myNumbers[i].length);
+      for(int j = 0; j < myNumbers[i].length; ++j) {
+        System.out.println(myNumbers[i][j]);
+      }
+    }
+  }
+}
